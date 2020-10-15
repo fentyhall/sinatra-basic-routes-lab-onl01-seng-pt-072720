211a64299f7144a1cdf9aea4fa80043a44317906
fentyhall <fentymhall@gmail.com>
2020-10-15 13:35:58 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Fenty"
+    end 
+
+    get '/hometown' do
+        "My hometown is Jakarta"
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is Baby One More Time"
+    end 
 end
